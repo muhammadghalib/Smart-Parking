d821fee096e053315712b18faba1db565f990b91
Muhammad Ghalib <147825005+muhammadghalib@users.noreply.github.com>
2024-12-02 06:55:27 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello, Vercel!"
+# Menambah batasan ukuran data
+app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024  # Maksimum 10 MB
 
-if __name__ == "__main__":
+@app.route('/', methods=['POST'])
+def receive_data():
+    data = request.get_json()  # Mengambil data JSON yang dikirim
+    print(f"Data received: {data}")
+    return jsonify({"status": "Data received successfully!"})
+
+if __name__ == '__main__':
     app.run(debug=True)
