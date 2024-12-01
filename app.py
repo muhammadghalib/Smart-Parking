2a29035acf5cf8fcc417234933722b84e8847b3f
Muhammad Ghalib <147825005+muhammadghalib@users.noreply.github.com>
2024-12-02 05:46:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,13 +4,9 @@ app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def receive_data():
-    # Mengambil data JSON yang dikirim oleh ESP32
-    data = request.get_json()
+    data = request.get_json()  # Mengambil data JSON yang dikirim oleh ESP32
     print(f"Data received: {data}")
-    
-    # Mengembalikan respon sukses
     return jsonify({"status": "Data received successfully!"})
 
 if __name__ == '__main__':
-    # Flask akan mendengarkan semua koneksi di jaringan lokal, pastikan port 5000 terbuka
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True)
