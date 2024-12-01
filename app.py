c45fcaa363d7c6fc7e9b2b1c8924746702e1862f
Muhammad Ghalib <147825005+muhammadghalib@users.noreply.github.com>
2024-12-02 06:21:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 
 app = Flask(__name__)
 
+# Menambah batasan ukuran data
+app.config['MAX_CONTENT_LENGTH'] = 10 * 1024 * 1024  # Maksimum 10 MB
+
 @app.route('/', methods=['POST'])
 def receive_data():
-    data = request.get_json()  # Mengambil data JSON yang dikirim oleh ESP32
+    data = request.get_json()  # Mengambil data JSON yang dikirim
     print(f"Data received: {data}")
     return jsonify({"status": "Data received successfully!"})
 
